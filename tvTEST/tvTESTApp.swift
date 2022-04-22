d3aea65e9dbc2adb02f65206396d6de537036396
SebastianLjungman <sebastian.ljungman@eyevinn.se>
2022-04-22 15:55:44 +0200

    Initial Commit

---
+++ tvTEST/tvTESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  tvTESTApp.swift
+//  tvTEST
+//
+//  Created by Sebastian Ljungman on 2022-04-22.
+//
+
+import SwiftUI
+
+@main
+struct tvTESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
