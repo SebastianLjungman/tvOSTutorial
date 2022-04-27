27641e3b8e8376a03d917be7e53a8f767ef15422
SebastianLjungman <sebastian.ljungman@eyevinn.se>
2022-04-27 17:08:24 +0200

    Missing project.pbxproj: These files?

---
+++ tvTEST/Views/LandmarkDetail.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkDetail.swift
+//  tvTEST
+//
+//  Created by Sebastian Ljungman on 2022-04-26.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
